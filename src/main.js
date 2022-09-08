076add53c8d569fb8780064e85537aac28fd4fd2
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2022-09-08 11:12:25 +0200

    firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,4 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
 createApp(App).use(store).use(router).mount('#app')
