2679f67fd3697b7fcf9ccfe74a1c7375dd394d5e
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2023-08-15 11:00:05 +0200

    Restyling patch 8.15

---
+++ src/components/404page.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="image bg-dark">
+      <div class="page">
+        <img
+          src="https://postimg.cc/GBvQyyz0"
+          alt=""
+        />
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {};
+  </script>
+  <style scoped>
+  .image {
+    height: 93vh;
+  }
+  .page {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    height: 50%;
+  }
+  
+  /* Media Queries */
+  @media only screen and (max-width: 376px) {
+    img {
+      width: 100%;
+    }
+    div#footer {
+      margin-top: -39%;
+    }
+    .page {
+      padding-bottom: 150px;
+    }
+  }
+  </style>
---
+++ src/views/404page.vue
@@ -0,0 +1,12 @@
+<template>
+    <PageNotFound></PageNotFound>
+  </template>
+  <script>
+  import PageNotFound from "../components/404page.vue";
+  export default {
+    components: {
+      PageNotFound,
+    },
+  };
+  </script>
+  <style></style>
