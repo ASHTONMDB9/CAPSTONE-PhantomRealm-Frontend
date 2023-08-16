6bff9dac7e5680508d85d82be5ba62d35378a4a3
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2023-08-16 19:43:11 +0200

    license fix

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -4,7 +4,7 @@
     <h1 class="text-center text-light">Thank you for your purchase</h1>
   </div>
   <div>
-    <a href="../assets/Games/download.txt" download>
+    <a href="../assets/license.zip" download>
     <img src="../assets/Images/downloadbtn.png" alt="license" width="200" height="100">
   </a>
   </div>
