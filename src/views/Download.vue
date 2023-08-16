f789210e2120d67c62d3c54473068aeac2dd43cb
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2023-08-16 19:18:04 +0200

    New order system

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,14 +1,22 @@
 <template lang="">
+  <section id="license">
  <div>
     <h1 class="text-center text-light">Thank you for your purchase</h1>
   </div>
-  <a href='../assets/Games/License.txt' download><button>Download</button></a>
+  <div>
+    <a href="../assets/Games/License.txt" download>
+    <img src="https://postimg.cc/QBrMpfZN" alt="license" width="100" height="100">
+  </a>
+  </div>
+  </section>
 </template>
 <script>
 export default {
   
 }
 </script>
-<style scoped>
-  
+<style>
+  #license {
+    padding-top: 120px;
+}
 </style>
