a53b7bfa34c51e9962d72206a7ceefb3c1092e56
ASHTONMDB9 <ashtonabrahams563@gmail.com>
2023-08-23 11:19:45 +0200

    license fix III

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -3,9 +3,9 @@
  <div>
     <h1 class="text-center text-light">Thank you for your purchase</h1>
   </div>
-  <div>
+  <div class="d-block mx-auto">
     <a href="https://drive.google.com/file/d/15wi94jmaDP9KGmSxCYjIRBGkk0rGaami/view?usp=sharing" download target="_blank">
-    <img src="../assets/Images/downloadbtn.png" alt="license" width="200" height="100">
+    <img src="../assets/Images/downloadbtn.png" alt="license" width="200" height="200">
   </a>
   </div>
   </section>
